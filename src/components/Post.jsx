0b5ab9a0ca8f337278a767f4a9b000de3c758555
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-01-11 22:55:30 -0300

    estilizando componentes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,7 @@ export function Post() {
             className={styles.avatar}
             src="https://avatars.githubusercontent.com/u/75049024?v=4"
           />
-          <div className={styles.authorInfor}>
+          <div className={styles.authorInfo}>
             <strong>Guilherme Kunsch</strong>
             <span>Web Developer</span>
           </div>
@@ -18,23 +18,27 @@ export function Post() {
           Publicado a 1h
         </time>
       </header>
+
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
-
         <p>
           Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
           no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
         </p>
-
         <p>
           👉 <a href=""> jane.design/doctorcare</a>
         </p>
-
         <p>
           <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
           <a href="">#rocketseat</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu Feedback</strong>
+        <textarea placeholder="Deixe seu comentário"/>
+        <button type="submit">Comentário</button>
+      </form>
     </article>
   );
 }
