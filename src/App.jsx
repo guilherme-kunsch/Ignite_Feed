2c8aa4355a5a88822799101b0af54377b5cde2e7
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-01-17 22:20:02 -0300

    iterando sobre objetos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,37 @@ import { Comment } from "./components/Comment";
 import "./global.css";
 import styles from "./App.module.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/75049024?v=4',
+      name: 'Guilherme Künsch',
+      role: 'Desenvolvedor Web'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '<a href=""> jane.design/doctorcare</a>'}
+    ], 
+    publishAt: new Date('2024-01-13 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/75049024?v=4',
+      name: 'Guilherme Künsch',
+      role: 'Desenvolvedor Web'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: '<a href=""> jane.design/doctorcare</a>'}
+    ], 
+    publishAt: new Date('2024-01-09 20:00:00'),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -12,8 +43,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                author={post.author}
+                content={post.content}
+                publishAt={post.publishAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
